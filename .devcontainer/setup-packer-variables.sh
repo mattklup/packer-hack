f7bb7e0cb22496c1a7db6dbf915047d9a6805bc2
mattklup <53016897+mattklup@users.noreply.github.com>
2022-03-11 20:03:31 +0000

    Disable secret leak

--- .devcontainer/setup-packer-variables.sh
+++ .devcontainer/setup-packer-variables.sh
@@ -1,5 +1,6 @@
-export PKR_VAR_client_id=$AZURE_CLIENT_ID
-export PKR_VAR_client_secret=$AZURE_CLIENT_SECRET
-export PKR_VAR_subscription_id=$AZURE_SUBSCRIPTION_ID
-export PKR_VAR_tenant_id=$AZURE_TENANT_ID
 export PKR_VAR_resource_group=mattklup-packerdemo
+
+export PKR_VAR_tenant_id=$(echo $AZURE_SERVICE_PRINCIPAL | jq -r .tenantId)
+export PKR_VAR_subscription_id=$(echo $AZURE_SERVICE_PRINCIPAL | jq -r .subscriptionId)
+export PKR_VAR_client_id=$(echo $AZURE_SERVICE_PRINCIPAL | jq -r .clientId)
+export PKR_VAR_client_secret=$(echo $AZURE_SERVICE_PRINCIPAL | jq -r .clientSecret)
